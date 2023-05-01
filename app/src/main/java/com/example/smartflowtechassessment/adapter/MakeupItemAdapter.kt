7e97dc5305e9a7bb4aef5969c4765de674a179b9
Samuelungbede1 <samuel.onum1@gmail.com>
2023-05-01 12:16:55 +0100

    implemented glide to show image of make up items

--- app/src/main/java/com/example/smartflowtechassessment/adapter/MakeupItemAdapter.kt
+++ app/src/main/java/com/example/smartflowtechassessment/adapter/MakeupItemAdapter.kt
@@ -1,20 +1,25 @@
 package com.example.smartflowtechassessment.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.smartflowtechassessment.R
 import com.example.smartflowtechassessment.model.MakeUpProductsItem
 
 
-class MakeupItemAdapter(private val makeupItems: List<MakeUpProductsItem>) :
+class MakeupItemAdapter(private val makeupItems: List<MakeUpProductsItem>, private val context: Context) :
     RecyclerView.Adapter<MakeupItemAdapter.ViewHolder>() {
 
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val productName: TextView = itemView.findViewById(R.id.productNameTv)
+        val image: ImageView = itemView.findViewById(R.id.productLogoIv)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -26,6 +31,11 @@ class MakeupItemAdapter(private val makeupItems: List<MakeUpProductsItem>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val makeupProduct = makeupItems[position]
         holder.productName.text = makeupProduct.name
+        val imageUrl = makeupProduct.api_featured_image
+        Glide.with(context)
+            .load("https:$imageUrl")
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(holder.image)
     }
 
     override fun getItemCount(): Int = makeupItems.size
